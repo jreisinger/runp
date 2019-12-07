cb135ba48ebd213f066cfeff8aa970730d6db9db
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2019-12-07 09:07:16 +0100

    test commands to run without shell

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -31,3 +31,31 @@ func TestCommandPrepare(t *testing.T) {
 		t.Fatalf("CmdToShow is wrong")
 	}
 }
+
+func TestCommandPrepareNoShell(t *testing.T) {
+	noshell := true
+	stderrChan := make(chan string)
+	stdoutChan := make(chan string)
+
+	// empty command
+	cmd := ""
+	c := Command{CmdString: cmd, StdoutCh: stdoutChan, StderrCh: stderrChan, NoShell: noshell}
+	c.Prepare()
+	if c.CmdString != "" {
+		t.Fatalf("CmdString is not empty: %v", c.CmdString)
+	}
+	if c.CmdToShow != "" {
+		t.Fatalf("CmdToShow is not empty: %v", c.CmdToShow)
+	}
+
+	// basic command
+	cmd = "ls -l"
+	c = Command{CmdString: cmd, StdoutCh: stdoutChan, StderrCh: stderrChan, NoShell: noshell}
+	c.Prepare()
+	if c.CmdString != "ls -l" {
+		t.Fatalf("CmdString is wrong")
+	}
+	if c.CmdToShow != "ls -l" {
+		t.Fatalf("CmdToShow is wrong")
+	}
+}
